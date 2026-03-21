0449b2e9806262668060cb0fc71d23e176fed5c2
Yuvan <yuvansc7@gmail.com>
2026-03-21 17:46:23 +0530

    initial commit

---
+++ gait_asymmetry.m
@@ -0,0 +1,7 @@
+figure(1)
+plot(linear_acceleration_2026_03_21_13_12_16.az_m_s_2,YDataSource = 'linear_acceleration_2026_03_21_13_12_16.az_m_s_2');
+linkdata on;
+ylabel("az_m_s_2");
+title("az_m_s_2");
+legend("show");
+
