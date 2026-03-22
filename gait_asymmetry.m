5ae9a49e812658cd4faa965ed9a61d6d7ff08e35
Yuvan <yuvansc7@gmail.com>
2026-03-22 11:31:08 +0530

    Added gitignore and a generic LPF

--- gait_asymmetry.m
+++ gait_asymmetry.m
@@ -1,7 +1,29 @@
+close all
+clear all
+
+T = readtable("linear_acceleration_2026-03-21_13.12.16.csv");
+T = renamevars(T, ["aT_m_s_2_", "ax_m_s_2_", "ay_m_s_2_", "az_m_s_2_"], ["aT_m_s_2", "ax_m_s_2", "ay_m_s_2", "az_m_s_2"])
+
 figure(1)
-plot(linear_acceleration_2026_03_21_13_12_16.az_m_s_2,YDataSource = 'linear_acceleration_2026_03_21_13_12_16.az_m_s_2');
+plot(T.time,T.az_m_s_2,XDataSource = 'T.time',YDataSource = 'T.az_m_s_2');
 linkdata on;
+xlabel("time");
 ylabel("az_m_s_2");
-title("az_m_s_2");
+title("az_m_s_2 vs time");
 legend("show");
 
+
+acc_z = T.az_m_s_2
+
+Ws = 100;
+Wc = 3;
+[b, a] = butter(4, Wc/(Ws/2), 'low');
+acc_z_filt = filtfilt(b, a, acc_z);
+
+figure(2)
+plot(T.time, acc_z, 'r'); hold on;
+plot(T.time, acc_z_filt, 'b');
+xlabel("time");
+ylabel("az_m_s_2");
+title("az_m_s_2 (filtered) vs time");
+legend('az_m_s_2', 'az_m_s_2 after LPF');
