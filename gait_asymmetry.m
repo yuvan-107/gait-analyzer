4329f7e7d3def2b5c34dc05db76f7d162c121bcf
Yuvan <yuvansc7@gmail.com>
2026-03-23 22:39:57 +0530

    implemented CNT value for the signal

--- gait_asymmetry.m
+++ gait_asymmetry.m
@@ -16,7 +16,7 @@ acc_z = T.az_m_s_2
 
 Ws = 100;
 Wc = 3;
-[b, a] = butter(4, Wc/(Ws/2), 'low');
+[b, a] = butter(4, Wc/(Ws/2), 'low'); %TODO: tweak filter design
 acc_z_filt = filtfilt(b, a, acc_z);
 
 figure(2)
@@ -30,7 +30,7 @@ legend('az_m_s_2', 'az_m_s_2 after LPF');
 min_acc = min(acc_z_filt);
 max_acc = max(acc_z_filt);
 
-norm_acc = (acc_z_filt - min_acc) / (max_acc - min_acc);
+norm_acc = (acc_z_filt - min_acc) / (max_acc - min_acc); %min max normalization
 
 figure(3)
 plot(T.time, norm_acc)
@@ -38,5 +38,36 @@ xlabel("time")
 ylabel("az_m_s_2")
 title("az_m_s_2 (filtered, normalized) vs time")
 legend("norm az_m_s_2")
+
+% norm_acc = trimdata(norm_acc,50,Side="both");
+% time = trimdata(T.time, 50, Side="both");
+% 
+% figure(4)
+% plot(time, norm_acc)
+% xlabel("time")
+% ylabel("az_m_s_2")
+% title("az_m_s_2 (filtered, normalized, trimmed) vs time")
+% legend("norm az_m_s_2")
+
+time = T.time;
+
+[ACF_1, shifts]= xcorr(norm_acc, 'normalized')
+
+figure(5)
+plot(shifts, ACF_1)
+xlabel("Shifts")
+ylabel("ACF (normalized)")
+title("Autocorrelation of Normalized Acceleration")
+legend("ACF")
+grid on;
+
+p_shifts = shifts(shifts > 0)'; %taking transpose cuz its row vector
+p_ACF = ACF_1(shifts > 0);
+
+[pks locs] = findpeaks(p_ACF, p_shifts, "MinPeakDistance", 30); %TODO: give tweaks in 4th parameter here
+CNT = locs(1) %locs(0) is when shift l = 0 so take next highest
+
+t_full_cycle = CNT/100; %time taken to make one full gait cycle (fs = 100 Hz)
+
 
 
